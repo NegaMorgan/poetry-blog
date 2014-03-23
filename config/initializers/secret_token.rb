37af5636a2cb9661cbc7a5aa58ec7a193436dacc
Morgan Evans <morganrevans@gmail.com>
2014-03-23 08:37:55 -0400

    Added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PoetryBlog::Application.config.secret_key_base = '44be02cd092cedee7961ad78ea0002249757cd9fa86c89ff306f79c5abb537b74878e8252a9d56dcffd0dd3fa826a1b41383828eb89fd179d5369d36bbe3dc06'
